40d59e62475a57f572cd710acb931b2fbf987502
nicmlu <NicoleMLucien@gmail.com>
2020-01-12 09:53:28 -0500

    creates a student with two attributes and saves it to the table

--- lib/student.rb
+++ lib/student.rb
@@ -53,4 +53,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
 
+  def self.create(name, grade) 
+    new_student = Student.new(name, grade)
+    new_student.save
+    new_student 
+  end 
+
 end
