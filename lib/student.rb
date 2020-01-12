93289f84ce3ae74fde0107742315c30580e44873
nicmlu <NicoleMLucien@gmail.com>
2020-01-12 09:50:29 -0500

    saves an instance of the class and assigns an id attribute, updates record if object already saved

--- lib/student.rb
+++ lib/student.rb
@@ -33,4 +33,24 @@ class Student
     DB[:conn].execute(sql)
   end 
 
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL 
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+     end
+  end 
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end 
+
 end
